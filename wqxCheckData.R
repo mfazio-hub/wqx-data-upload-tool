cfbf02c313ac5c72e540d8dc4dff701a90b9c74a
Mike Fazio <michaelf@santarosa.fl.gov>
2025-10-08 14:45:58 -0500

    First Commit

---
+++ wqxCheckData.R
@@ -0,0 +1,10 @@
+# Use this to check what data is already in WQX
+df <- dataRetrieval::readWQPdata(project = "SRC-SRSWQ", service = "ResultWQX3", dataProfile = "basicPhysChem")
+
+# View unique dates
+unique(df$Activity_StartDate)
+
+
+sel <- df %>% select(c(Result_Characteristic, Result_MeasureUnit))
+
+uniques <- unique(df[, c("Result_Characteristic", "Result_MeasureUnit")])
