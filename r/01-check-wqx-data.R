eadf9ea2532e82b7f7f7e2241567e6a23db83177
Mike Fazio <michaelf@santarosa.fl.gov>
2025-10-09 16:01:19 -0500

    added AGOL data retrieval, formatted code

--- r/01-check-wqx-data.R
+++ r/01-check-wqx-data.R
@@ -1,5 +1,9 @@
 # Use this to check what data is already in WQX
-df <- dataRetrieval::readWQPdata(project = "SRC-SRSWQ", service = "ResultWQX3", dataProfile = "basicPhysChem")
+df <- dataRetrieval::readWQPdata(
+  project = "SRC-SRSWQ",
+  service = "ResultWQX3",
+  dataProfile = "basicPhysChem"
+)
 
 # View unique dates
 unique(df$Activity_StartDate)
